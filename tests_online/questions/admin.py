e430207f363139ee742bf6b6c913c047e4bd379f
Bogdan Kalashnikov <bogdan.dm1995@yandex.ru>
2019-04-30 18:52:29 +0300

    Admin models

--- tests_online/questions/admin.py
+++ tests_online/questions/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+class QuestionAdminInline(admin.TabularInline):
+    model = models.Question
+
+@admin.register(models.Test)
+class TestAdmin(admin.ModelAdmin):
+    inlines = [QuestionAdminInline]
+
+
+
+class AnswerAdminInline(admin.TabularInline):
+    model = models.Answer
+
+@admin.register(models.Question)
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [AnswerAdminInline]
+
+
+@admin.register(models.Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.UserAnswers)
+class UserAnswersAdmin(admin.ModelAdmin):
+    pass
