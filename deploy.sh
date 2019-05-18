5d489ed48c33d60fb6bc632a58d1d87f2bf96b68
Bogdan Kalashnikov <bogdan.dm1995@yandex.ru>
2019-05-18 19:45:12 +0300

    Fix deploy ssh script

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 ./docker_push.sh $1
 
-ssh -i /tmp/id_rsa root@bogdandm.xyz << EOF
+ssh -o "StrictHostKeyChecking no" -tt -i /tmp/id_rsa root@bogdandm.xyz << EOF
     cd /opt/tests_online/tests_online_docker
     git pull
     docker-compose pull
