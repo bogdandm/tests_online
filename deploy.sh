b55f6b1778778cf275daf53cef08657a79aa66b2
Bogdan Kalashnikov <bogdan.dm1995@yandex.ru>
2019-05-18 20:41:25 +0300

    Fix docker-compose

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,6 @@
 ssh -o "StrictHostKeyChecking no" -T -i /tmp/id_rsa root@bogdandm.xyz << EOF
     cd /opt/tests_online/tests_online_docker
     git pull && \
-    docker-compose -f docker-compose-production.yml pull && \
-    docker-compose -f docker-compose-production.yml up -d
+    docker-compose -f docker-compose.yml -f docker-compose-production.yml pull && \
+    docker-compose -f docker-compose.yml -f docker-compose-production.yml up -d
 EOF
